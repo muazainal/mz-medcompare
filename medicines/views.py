6425e31f6a10b5c88b1028a5b42909fe674bf938
muazainal <muazainal@live.com>
2026-02-21 15:43:04 +0000

    Create medicine_detail view to render single medicine details

--- medicines/views.py
+++ medicines/views.py
@@ -1,16 +1,17 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404  # <-- added get_object_or_404
+from .models import Medicine
 
-# Create your views here.
-# def home(request):
-#     return HttpResponse("Welcome to MedCompare! 🚀")
-
-# Home page view
+# Home page view - list all medicines
 def home(request):
-    # This renders home.html for the root URL
-    return render(request, 'medicines/home.html')
+    medicines = Medicine.objects.all()  # fetch all medicines from DB
+    return render(request, 'medicines/home.html', {'medicines': medicines})
 
-# Medicines page view
-def medicines_list(request):
-    # This renders medicines_list.html for /medicines URL
-    return render(request, 'medicines/medicines_list.html')
+# Detail page for each medicine
+def medicine_detail(request, pk):
+    medicine = get_object_or_404(Medicine, pk=pk)
+    return render(request, 'medicines/medicine_detail.html', {'medicine': medicine})
+
+# # Medicines page view - optional, could be same as home
+# def medicines_list(request):
+#     medicines = Medicine.objects.all()  # fetch all medicines from DB
+#     return render(request, 'medicines/medicines_list.html', {'medicines': medicines})
