0bc0f905358d3c757b19aa0a299e477d19bf0719
muazainal <muazainal@live.com>
2026-02-28 12:28:57 +0000

    Update medicines views and template

--- medicines/views.py
+++ medicines/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render, get_object_or_404  # <-- added get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Medicine
 
-# Home page view - list all medicines
+
+# HOME PAGE
+# This function runs when someone visits "/"
+# It fetches all Medicine objects from the database
 def home(request):
-    medicines = Medicine.objects.all()  # fetch all medicines from DB
-    return render(request, 'medicines/home.html', {'medicines': medicines})
 
-# Detail page for each medicine
+    # Query the database table for Medicine
+    # Equivalent SQL: SELECT * FROM medicines_medicine;
+    medicines = Medicine.objects.all()
+
+    # Render means:
+    # 1. Take this template file
+    # 2. Insert data into it
+    # 3. Return HTML to the browser
+    return render(request,
+                  'medicines/medicines_list.html',
+                  {
+                      # This dictionary sends data into template
+                      # Inside template we can use: {% for med in medicines %}
+                      'medicines': medicines
+                  })
+
+
+# DETAIL PAGE
+# This runs when someone visits: /medicine/1/
 def medicine_detail(request, pk):
-    medicine = get_object_or_404(Medicine, pk=pk)
-    return render(request, 'medicines/medicine_detail.html', {'medicine': medicine})
 
-# # Medicines page view - optional, could be same as home
-# def medicines_list(request):
-#     medicines = Medicine.objects.all()  # fetch all medicines from DB
-#     return render(request, 'medicines/medicines_list.html', {'medicines': medicines})
+    # Get medicine by primary key (id)
+    # If not found → show 404 error
+    medicine = get_object_or_404(Medicine, pk=pk)
+
+    return render(request,
+                  'medicines/medicine_detail.html',
+                  {
+                      'medicine': medicine
+                  })
