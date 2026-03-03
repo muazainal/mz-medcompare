770093df8ecce4df0438614e2c2b1b8013c4a652
muazainal <muazainal@live.com>
2026-03-03 13:52:34 +0000

    feat: Add `formula` CharField directly to the Medicine model and update its display and search functionality in templates and admin.

--- medicines/models.py
+++ medicines/models.py
@@ -24,6 +24,7 @@ class Medicine(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     dosage = models.CharField(max_length=255, blank=True, null=True)
+    formula = models.CharField(max_length=255, blank=True, null=True)
     rating = models.FloatField(default=0)
     manufacturer = models.ForeignKey(
         Manufacturer,
