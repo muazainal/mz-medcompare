34cba3631b7cad34377fb0939ec1f96a69294108
muazainal <muazainal@live.com>
2026-03-06 19:06:36 +0000

    Remove .env from repository and ignore it

--- medicines/admin.py
+++ medicines/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Manufacturer, Formula, Medicine, LabReport
+from .models import Manufacturer, Formula, Medicine, LabReport, Order
 
 # Register your models here.
 admin.site.register(Manufacturer)
@@ -17,3 +17,9 @@ class LabReportAdmin(admin.ModelAdmin):
     list_display = ('medicine', 'pdf_file', 'uploaded_at')
     list_filter = ('uploaded_at',)
     search_fields = ('medicine__name',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'medicine', 'amount', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__username', 'medicine__name', 'stripe_checkout_id')
