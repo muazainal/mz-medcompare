f6229bcec3f3b3b5c6fee7489c6c39cb218745db
muazainal <muazainal@live.com>
2026-03-07 17:42:51 +0000

    feat: Configure database using environment variables and refine user greeting display in base template.

--- medcompare_project/settings.py
+++ medcompare_project/settings.py
@@ -85,6 +85,13 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
