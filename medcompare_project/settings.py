8b28adea22a9a18d33ae5571df65c17ba8aeaebc
muazainal <muazainal@live.com>
2026-03-07 18:27:44 +0000

    refactor: Update database configuration to use individual environment variables for PostgreSQL instead of `dj_database_url`.

--- medcompare_project/settings.py
+++ medcompare_project/settings.py
@@ -85,12 +85,17 @@ DATABASES = {
     }
 }
 
-import dj_database_url
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+if os.getenv('DB_NAME'):
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD':  os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
 
 
 # Password validation
