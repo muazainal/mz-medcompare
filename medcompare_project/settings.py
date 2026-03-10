956500c8121d61281d6d96975d942c8bd6766e5d
muazainal <muazainal@live.com>
2026-03-10 17:19:42 +0000

    Allow DEBUG to be controlled by environment variable for troubleshooting

--- medcompare_project/settings.py
+++ medcompare_project/settings.py
@@ -28,8 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-hidden-for-safety-replace-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*', '.herokuapp.com']
 
